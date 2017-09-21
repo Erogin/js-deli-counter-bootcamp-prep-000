6c66ff7b30cd1dd9efd39f134962c1ae91b0ec99
Eric Roginek <eroginek@protonmail.com>
2017-09-21 19:47:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+var katzDeli = []
+
+function takeANumber(katzDeliLine, name){
+  var katzDeli = katzDeli[katzDeliLine]
+    for(let i = 0; i<katzDeliLine; i++){
+    katzDeli[i] = katzDeli.push(name)
+    }
+  return katzDeli
+}
